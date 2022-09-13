58ccd613a19fe5c8fe97f107bac71ee0bb3485a8
Mykola <nickkosh@gmail.com>
2022-09-13 15:13:57 +0200

    function implementation

--- timerWithDelay/main.swift
+++ timerWithDelay/main.swift
@@ -5,7 +5,26 @@
 //  Created by Mykola on 13.09.2022.
 //
 
+//required Framework
 import Foundation
 
-print("Hello, World!")
-
+func timerWithDelay(_ : String) {
+    //any label on screen
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    //change text to empty string
+    titleLabel.text = ""
+    
+    //property for delay
+    var charIndex = 0.0
+    
+    //text to want loop
+    let titleText = "⚡️FlashChat"
+    for char in titleText {
+        //use func Timer
+        Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+            self.titleLabel.text?.append(char)
+        }
+        charIndex += 1
+    }
+}
